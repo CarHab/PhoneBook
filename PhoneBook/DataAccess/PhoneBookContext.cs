da192589e87568cabdea452143aaf366ca2f6350
Carlos Eduardo Habib Baptista Lima <carloshabib03@gmail.com>
2023-03-08 17:30:25 -0300

    Add project files.

---
+++ PhoneBook/DataAccess/PhoneBookContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+
+namespace PhoneBook.DataAccess;
+public class PhoneBookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PhoneBook;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+}
---
+++ PhoneBook/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Models;
+public class Contact
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName="varchar(200)")]
+    public string Name { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(50)")]
+    public string PhoneNumber { get; set; }
+}
